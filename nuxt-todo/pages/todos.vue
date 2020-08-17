63591bdce4f3af51dbc44844f212acebbd8c217f
nmurata0418 <nmurata@skywill.com>
2020-08-17 16:11:16 +0900

    ccc

---
+++ nuxt-todo/pages/todos.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <!-- {{ todos }} -->
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                <!-- {{ todo }} -->
+                {{ todo.done }} {{ todo.name }} {{ todo.created }}
+            </li>
+        </ul>
+        <div class="form">
+            <form v-on:submit.prevent="add">
+                <input v-model="name">
+                <button>Add</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            name: '',
+            done: false
+        }
+    },
+    created: function() {
+        this.$store.dispatch('todos/init')
+    },
+    methods: {
+        add() {
+            this.$store.dispatch('todos/add', this.name)
+            this.name = ''
+        }
+    },
+    computed: {
+        todos() {
+            return this.$store.state.todos.todos
+        }
+    }
+}
+</script>
